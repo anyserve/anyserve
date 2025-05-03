055c6fb946a8254ff6e05f8ebb3c9b14d5f03289
arcosx <arcosx@outlook.com>
2025-05-03 14:28:04 +0800

    🔧 Improve logging and error handling in InferenceService

--- anyserve/pkg/grpc_service/fetch_infer.go
+++ anyserve/pkg/grpc_service/fetch_infer.go
@@ -1,21 +1,25 @@
 package grpc_service
 
 import (
+	"fmt"
+
 	"github.com/anyserve/anyserve/pkg/proto"
+	"go.uber.org/zap"
 )
 
-// FetchInfer implements the FetchInfer RPC method
 func (s *InferenceService) FetchInfer(req *proto.FetchInferRequest, stream proto.GRPCInferenceService_FetchInferServer) error {
 	ctx := stream.Context()
 
 	responseChan, err := s.meta.PopInferRequest(ctx, req.Metadata)
 	if err != nil {
+		logger.Error("Failed to Pop Infer Request", zap.Error(err))
 		return err
 	}
 
 	for response := range responseChan {
-		logger.Sugar().Debugf("FetchInfer response: %v", response)
+		logger.Debug(fmt.Sprintf("Fetch Infer Request: %v", response))
 		if err := stream.Send(response); err != nil {
+			logger.Error("Failed to Send Fetch Infer Request", zap.Error(err))
 			return err
 		}
 	}
--- anyserve/pkg/grpc_service/infer_stream.go
+++ anyserve/pkg/grpc_service/infer_stream.go
@@ -1,6 +1,7 @@
 package grpc_service
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/anyserve/anyserve/pkg/proto"
@@ -11,23 +12,26 @@ import (
 // InferStream implements the InferStream RPC method
 func (s *InferenceService) InferStream(stream proto.GRPCInferenceService_InferStreamServer) error {
 	requestID := uuid.New().String()
-
 	_logger := logger.With(zap.String("request_id", requestID))
+
 	for {
 		req, err := stream.Recv()
 		if err == io.EOF {
+			_logger.Debug("Received infer request: EOF")
 			return nil
 		}
 		if err != nil {
+			_logger.Error("Received infer request error", zap.Error(err))
 			return err
 		}
 
-		_logger.Info("Received InferStream request", zap.Any("request", req))
+		_logger.Debug(fmt.Sprintf("Received infer request: %v", req))
 
 		if err := stream.Send(&proto.InferResponse{
 			RequestId: requestID,
 			Response:  &proto.InferCore{Content: []byte("processed stream output")},
 		}); err != nil {
+			_logger.Error("Failed to send infer response", zap.Error(err))
 			return err
 		}
 	}
